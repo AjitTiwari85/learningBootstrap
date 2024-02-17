03de291a02e46170ad5ddc1cb916263d62ac16b2
Rahul Tiwari <at856575@gmail.com>
2024-02-17 13:24:15 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return  (
+    <>
+  <div>
+    this is mycghghghghgh
+  </div><div>
+    this is mycghghghghgh
+  </div><div>
+    this is mycghghghghgh
+  </div><div>
+    this is mycghghghghgh
+  </div>
+  </>
+  )
+      
+    
+  
+}
+
+export default App
